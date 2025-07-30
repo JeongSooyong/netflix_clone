c9308d50b7fe3dc5be2fbe2f56092156332eb441
JeongSooYong <bcjsy@naver.com>
2025-07-30 10:25:16 +0900

    장르 매퍼, vo 완료, 반복문으로 html에 장르 전체 띄우기 부터. 2025-07-30

---
+++ src/main/java/com/soo/netflix_clone/vo/Genre.java
@@ -0,0 +1,32 @@
+package com.soo.netflix_clone.vo;
+
+public class Genre {
+
+    private int genreNo;
+    private String genreName;
+
+    public int getGenreNo() {
+        return this.genreNo;
+    }
+
+    public void setGenreNo(int genreNo) {
+        this.genreNo = genreNo;
+    }
+
+    public String getGenreName() {
+        return this.genreName;
+    }
+
+    public void setGenreName(String genreName) {
+        this.genreName = genreName;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " genreNo='" + getGenreNo() + "'" +
+            ", genreName='" + getGenreName() + "'" +
+            "}";
+    }
+
+}
