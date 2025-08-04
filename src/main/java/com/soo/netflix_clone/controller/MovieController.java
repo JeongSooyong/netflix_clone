e5658655af1eb3e8e280d27811eae643cf1e5573
JeongSooYong <bcjsy@naver.com>
2025-08-04 23:07:11 +0900

    updateMovie getmapping, 뷰까지 완료. Mapping, service, dao부터 시작 2025-08-04

--- src/main/java/com/soo/netflix_clone/controller/MovieController.java
+++ src/main/java/com/soo/netflix_clone/controller/MovieController.java
@@ -44,8 +44,8 @@ public class MovieController {
     @Autowired
     private GenreServiceImpl genreService;
 
-    // application.properties의 'file.upload-dir' 값을 주입받음
-    // 이 경로는 File 객체 생성 시 문자열로 사용됩니다.
+    // application.properties의 'file.upload-dir' 값을 주입
+    // 이 경로는 File 객체 생성 시 문자열로 사용
     @Value("${file.upload-dir}")
     private String uploadDir;
 
@@ -143,23 +143,28 @@ public class MovieController {
             return "redirect:/insertMovie";
         }
     
+        // List형태로 받은 장르에 값이 할당 되었는지 확인하는 조건문
         if (selectedGenreNos != null && !selectedGenreNos.isEmpty()) {
+            // 장르가 선택되었다면, 리스트의 첫 번째 인덱스 장르 ID를 MovieVo 객체의 movieGenre 필드에 설정
             movieVo.setMovieGenre(selectedGenreNos.get(0)); 
-        } else {
+        } else { // List형태로 받은 장르가 비어있을 때
+             // 오류 메시지를 flash attribute에 담아, 리다이렉트 후 사용자에게 전달
             redirectAttributes.addFlashAttribute("errorMsg", "장르를 하나 이상 선택해주세요.");
             return "redirect:/insertMovie";
         }
 
         try {
+            // movieVo클래스의 commonNo에 201을 할당.
             movieVo.setCommonNo(201); 
 
+            // 서비스 계층의 insertMovie메서드 실행
             int result = movieService.insertMovie(movieVo); 
             
-
+            // result가 0보다 클 경우(영화가 성공적으로 등록되었을 경우)
             if (result > 0) {
                 redirectAttributes.addFlashAttribute("successMsg", "영화가 성공적으로 등록되었습니다!");
                 return "redirect:/main";
-            } else {
+            } else { // 그렇지 않을 경우(영화 등록에 실패했을경우)
                 redirectAttributes.addFlashAttribute("errorMsg", "영화 등록에 실패했습니다.");
                 return "redirect:/insertMovie";
             }
@@ -170,18 +175,23 @@ public class MovieController {
         }
     }
 
-
+    // 영화 상세 보기 페이지
     @GetMapping("/selectMovie/{movieTitle}")
     public String selectMovie(@PathVariable("movieTitle") String movieTitle, Model model, HttpSession session) {
 
+        // 로그인된 세션 UserVo를 가져오는 loginUser
         UserVo loginUser = (UserVo) session.getAttribute("loginUser");
         model.addAttribute("loginUser", loginUser);
 
+        // 조회할 MovieVo 객체를 담을 변수 vo
         MovieVo vo = new MovieVo();
+        // 변수 vo에 영상 제목을 할당
         vo.setMovieTitle(movieTitle);
 
+        // 서비스 계층의 selectMovie메서드 실행하여 movie라는 변수에 할당
         MovieVo movie = movieService.selectMovie(movieTitle); 
 
+        // movie가 null일때(영상이 선택되지 않았을때)
         if (movie == null) {
             model.addAttribute("errorMsg", "해당 영화 정보를 찾을 수 없습니다.");
             return "redirect:/main";
@@ -190,6 +200,22 @@ public class MovieController {
         model.addAttribute("movie", movie);
 
         return "selectMovie";
+    }
+
+    @GetMapping("/updateMovie/{movieTitle}")
+    public String updateMovie(@PathVariable("movieTitle") String movieTitle, HttpSession session, Model model) {
+
+        UserVo loginUser = (UserVo) session.getAttribute("loginUser");
+        model.addAttribute("loginUser", loginUser);
+
+        MovieVo movie = movieService.selectMovie(movieTitle);
+
+        model.addAttribute("movie", movie); 
+
+        List<GenreVo> genres = genreService.selectAllGenres();
+        model.addAttribute("genres", genres);
+
+        return "updateMovie";
     }
 
 }
